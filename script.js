5fb81af90efa9e405bf1e9331a97b49f651eba4e
Ethan Chow <112028216+ngyesh8@users.noreply.github.com>
2025-11-23 17:47:34 -0800

    particle buffer test

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ class Particle {
     constructor(effect){
         this.effect = effect;
         this.radius = Math.floor(Math.random() * 28 + 10); //particle size
-        this.buffer = this.radius * 4;
+        this.buffer = this.radius * 2;
         this.x = this.radius + Math.random() * (this.effect.width - this.radius * 2);
         this.y = this.radius + Math.random() * (this.effect.height - this.radius * 2);
         this.vx = Math.random() * 1 - 0.5;
